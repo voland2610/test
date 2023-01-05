8290750f23fce0e2a1c9395b65dd568dd9896f7c
foreanqq <sasha3222233@gmail.com>
2023-01-05 12:00:36 +0300

    12

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 new Swiper(".galary_image", {
   slidesPerView: 4,
 });
-
-if (iosVersion >= 7) {
-  $(document).scroll(function () {
-    $("#main").css(
-      "background-position",
-      "0px " + $(document).scrollTop() + "px"
-    );
-  });
-}
